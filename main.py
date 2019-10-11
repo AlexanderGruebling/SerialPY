5e5fabae81dec29d67d8e5fafe108798d52406c6
Rafael <6136@htl.rennweg.at>
2019-10-11 15:54:37 +0200

    added getUsbEnpoints

--- main.py
+++ main.py
@@ -23,24 +23,29 @@ window.geometry('350x200')
 # activate GUI Loop
 window.mainloop()
 
+def getUsbEnpoints():
+    dev = usb.core.find(idVendor=idVendor, idProduct=idProduct)
+    endpoint = dev[0][(0, 0)][0]
+    return endpoint
 
 
-dev = usb.core.find(idVendor=idVendor, idProduct=idProduct) #idVendor=0x0461,idProduct=0x4d0f
-interface = 0
-print(dev[0][(0,0)][0])
-endpoint = dev[0][(0,0)][0]
 
-dev.set_configuration()
-while True:
-    try:
-        data = dev.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
-        print(data)
-    except usb.core.USBError as e:
-        if e.args == ('Operation timed out',):
-            continue
+#dev = usb.core.find(idVendor=idVendor, idProduct=idProduct) #idVendor=0x0461,idProduct=0x4d0f
+#interface = 0
+#print(dev[0][(0,0)][0])
+#endpoint = dev[0][(0,0)][0]
+
+#dev.set_configuration()
+#while True:
+ #   try:
+  #      data = dev.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
+   #     print(data)
+    #except usb.core.USBError as e:
+     #   if e.args == ('Operation timed out',):
+      #      continue
 # release the device
-usb.util.release_interface(dev, interface)
-usb.util.dispose_resources(dev)
+#usb.util.release_interface(dev, interface)
+#usb.util.dispose_resources(dev)
 # reattach the device to the OS kernel
 # dev.attach_kernel_driver(interface)
 
--- usbPortId.py
+++ usbPortId.py
@@ -2,9 +2,9 @@ import usb.core
 import usb.util
 
 # find our device
-dev = usb.core.find(idVendor=0x0461,idProduct=0x4d0f)
+dev = usb.core.find()
 print(dev)
-print("----------------------------------------")
-print(dev[0][(0,0)][0])
-print("----------------------------------------")
-dev.is_kernel_driver_active(0)
+#print("----------------------------------------")
+#print(dev[0][(0,0)][0])
+#print("----------------------------------------")
+#dev.is_kernel_driver_active(0)
