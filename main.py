d40ee1b40f96975d2099f8a0f1727e8cb8ec68d0
AlexanderGruebling <6138@htl.rennweg.at>
2019-10-18 17:19:26 +0200

    Created toHex and toAscii function, created gitignore

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import usb.util
 from tkinter import *
 import _thread
 import threading
+from Operations import to_hex, to_ascii
 from Endpoint import Endpoint
 
 # global Settings
-idVendor = 0x0461
-idProduct = 0x4d0f
+idVendor = 0x0738
+idProduct = 0x1703
 baudRate = 9600
 dataBits = 8
 stopBits = 1
@@ -19,10 +20,12 @@ endpoint = Endpoint(idVendor, idProduct)
 
 # _thread.start_new_thread(endpoint.readFromSerialPort)
 
-thread1 = threading.Thread(target=endpoint.readFromSerialPort,args=(0,))
+thread1 = threading.Thread(target=endpoint.readFromSerialPort, args=(0,))
 thread1.start()
 
 print("test")
+print(to_hex(10))
+print(to_ascii(97))
 #dev = usb.core.find(idVendor=idVendor, idProduct=idProduct) #idVendor=0x0461,idProduct=0x4d0f
 #interface = 0
 #print(dev[0][(0,0)][0])
